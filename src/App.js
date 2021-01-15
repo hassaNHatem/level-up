6c57be034028490bacf5dbd01e2c30a84f7f3b88
Muhammed Ebrahim <54213690+MuEbrahim@users.noreply.github.com>
2021-01-15 15:46:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Shop from './components/ShopComponents/Shop'
 
 function App() {
   return (
     <div className="App">
-      <button>Show more</button>
+      <Shop/>
     </div>
   );
 }
