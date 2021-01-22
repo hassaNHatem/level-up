1721306c4d91c80e7fdde6adf3a15a79bc4de360
hassaNHatem <62301740+hassaNHatem@users.noreply.github.com>
2021-01-22 19:55:58 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Home from './homeComponents/Home'
-
+import Navbar from './homeComponents/Navbar'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import Shop from './components/ShopComponents/Shop';
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Route path="/" exact>
       <Home></Home>
+      </Route>
+      <Route path="/shop">
+        <Navbar></Navbar>
+    <Shop></Shop>
+      </Route>
     </div>
+    </Router>
   );
 }
 
